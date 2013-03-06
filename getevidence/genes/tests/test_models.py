16907976915bfb8abab413a3f7ec9ccd52f7886e
Madeleine Ball <mpball@gmail.com>
2013-03-05 21:43:54 -0500

    Add gene model tests

--- getevidence/genes/tests/test_models.py
+++ getevidence/genes/tests/test_models.py
@@ -3,11 +3,22 @@ Tests models.py in genes app.
 """
 
 from django.test import TestCase
+from ..models import Gene
 
+class GenesModelsTest(TestCase):
+    """Tests models in models.py"""
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+        Gene.objects.get_or_create(hgnc_name='HBB',
+                                   genetests=True,
+                                   genereviews=True)
+
+    def test_Gene_create_with_JAK2(self):
+        """Tests Gene creation."""
+        Gene.objects.create(hgnc_name='JAK2',
+                            genetests=True,
+                            genereviews=False)
+
+    def test_Gene_delete_with_HBB(self):
+        """Tests Gene deletion."""
+        Gene.objects.get(hgnc_name='HBB').delete()
