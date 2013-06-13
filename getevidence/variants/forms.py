63f174c4d165c1d7d895040c4606852a1cafa9a6
Madeleine Ball <mpball@gmail.com>
2013-06-13 17:51:34 -0400

    Improve variant display

--- getevidence/variants/forms.py
+++ getevidence/variants/forms.py
@@ -4,3 +4,22 @@ from django import forms
 class VariantReviewForm(forms.ModelForm):
     class Meta:
         model = VariantReview
+        fields = ('review_summary',
+                  'review_long',
+                  'impact',
+                  'inheritance',
+                  'evidence_computational',
+                  'evidence_functional',
+                  'evidence_casecontrol',
+                  'evidence_familial',
+                  'clinical_severity',
+                  'clinical_treatability',
+                  'clinical_penetrance',
+                  )
+
+        widgets = {
+            'review_summary': forms.Textarea(attrs={'class': 'span10 lead',
+                                                    'rows': 3}),
+            'review_long': forms.Textarea(attrs={'class': 'span10',
+                                                 'rows': 8})
+            }
