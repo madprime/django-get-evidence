b9564fbda3427e493fdcde5106a6edf6458efc52
Madeleine Ball <mpball@gmail.com>
2013-09-13 10:45:40 -0400

    Update publication PubMed data pull and display

--- getevidence/publications/models.py
+++ getevidence/publications/models.py
@@ -1,7 +1,7 @@
-import requests
+import re
+from Bio import Entrez
 from django.db import models
 import reversion
-import re
 
 class Publication(models.Model):
     """Records publication information.
@@ -12,7 +12,12 @@ class Publication(models.Model):
 
     """
     pmid = models.CharField(max_length=16, unique=True)
-    pubmed_text = models.TextField()    
+    author_list = models.TextField(blank=True)
+    title = models.TextField()
+    pub_date = models.CharField(max_length=18)
+    journal = models.CharField(max_length=30)
+    journal_location = models.CharField(max_length=25)
+    abstract = models.TextField(blank=True)
 
     def __unicode__(self):
         """Returns string with Pubmed ID."""
@@ -21,7 +26,7 @@ class Publication(models.Model):
     @classmethod
     def create(cls, pmid=None):
         pub = cls(pmid=pmid)
-        pub.get_pubmed_text()
+        pub.get_pubmed_data()
         return pub
 
     @classmethod
@@ -30,47 +35,54 @@ class Publication(models.Model):
         pub_match = cls.objects.get(pmid__exact=pmid)
         return pub_match
 
-    def get_pubmed_text(self):
-        """Pull abstract text from NCBI."""
-        req = requests.get('http://www.ncbi.nih.gov/pubmed/' + 
-                           self.pmid + '?format=text')
-        if req.status_code == 200:
-            self.pubmed_text = req.text
+    def get_pubmed_data(self):
+        """Pull and parse abstract text from NCBI."""
+        Entrez.email = "mad-getevidence@printf.net"
+        handle = Entrez.efetch(db="pubmed", id=self.pmid, retmode="xml")
+        record = Entrez.parse(handle).next()
 
-    def pubmed_text_parse(self):
-        """Parse important information from PubMed text"""
-        betweenpre = re.compile("(<pre>\n\d{1}.\s{1})(.+)(\[PubMed.+\])", flags=re.DOTALL)
-        split_text = betweenpre.search(f).groups()
-        split_nopre = split_text[1]
-        split_nolinebreak = split_nopre.strip().replace("\n\n", " ").replace("\n"," ")
-        split_everything = re.compile(
-            """(?P<journal>^[A-Z]{1}[a-z]*\s*[A-Z]*[a-z]*\s*[A-Z]*[a-z]*) # Matches journal name
-                                        \.\s{0,1}
-                                        (?P<publishing_date>(19|20)[0-9]{2}\s{0,1}[A-Z]{0,1}[a-z]{0,2}\s{0,1}[0-9]{0,2}) # Matches publishing date
-                                        ;\s{0,1}
-                                        (?P<volume_and_edition>[0-9]+\([0-9]+\)) # Matches volume and edition(?)
-                                        :
-                                        (?P<page_numbers>[0-9]+-[0-9]*) # Matches page numbers
-                                        \.\s{0,1}
-                                        (doi:\s)?(?P<doi>[0-9a-z\./]+.\s)? # Matches digital object identifier, if given
-                                        (Epub)?\s?(?P<EPub_date>19|20[0-9]{2}\s?[A-Z]{1}?[a-z]{2}?\s?[0-9]{1,2}?\.\s{0,1})? # Matches epublishing info, if given
-                                        (?P<article_title>.+?) # Matches article title
-                                        \.\s{0,1}
-                                        (?P<authors>[A-Z\S]{1}[A-Za-z-\S]+\s{1}[A-Z\S]{1,2}\s?[A-Za-zI-III]{0,3}(,\s{1}[A-Z\S]{1}[A-Za-z-\S]+\s{1}[A-Z\S]{1,2}\s?[A-Za-zI-III]{0,3})*?) #Matches list of authors
-                                        \.\s{0,1}
-                                        (?P<lab_info>(Department|Center)?.+\s[0-9]+?-?[0-9]*?,\s[A-Z]+?) # Matches University\lab information
-                                        \.\s{0,1}
-                                        (Comment.+?\.\s19|20[0-9]{2}\s?[A-Z]{1}?[a-z]{2}?\s?[0-9]{1,2}?;?[0-9]+?\(?[0-9]*?\)?:?[0-9]*?-?[0-9]*?\.\s)? # Matches comment info, if given
-                                        (?P<email>[A-Za-z0-9]+(\.[A-Za-z0-9]+)*@[A-Za-z0-9]+?(\.[A-Za-z0-9]+)*?.(edu|com|net|org))? # Matches contact e-mail, if given
-                                        \s{0,1}
-                                        (?P<abstract>.+) # Matches abstract
-                                        \.\s?(PMCID)?:?\s?
-                                        (?P<PMCID>PMC[0-9]+)? # Matches PMCID
-                                        \s?PMID:\s{0,1}
-                                        (?P<PMID>[0-9]+$) # Matches PMID
-                                        """
-                                        ,re.X|re.UNICODE)
-        info_dictionary = split_everything.search(split_nolinebreak).groupdict()
-        return info_dictionary
+        # Author data
+        authors = record['MedlineCitation']['Article']['AuthorList']
+        self.author_list = ', '.join([a['LastName'] + ' ' + a['Initials']
+                                 for a in authors[0:10]])
+        if len(authors) > 10:
+            self.author_list += ', et al.'
+        else:
+            self.author_list += '.'
+
+        # Article title
+        self.title = record['MedlineCitation']['Article']['ArticleTitle']
+
+        # Date data
+        date_data = record['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']
+        self.pub_date = date_data['Year']
+        if 'Month' in date_data:
+            self.pub_date += ' ' + date_data['Month']
+            if 'Day' in date_data:
+                self.pub_date += ' ' + date_data['Day']
+
+        # Journal name (abbreviated)
+        self.journal = record['MedlineCitation']['Article']['Journal']['ISOAbbreviation']
+        self.journal = re.sub('\.','', self.journal)
+
+        # Journal article location (volume/issue/pagination)
+        journal_issue_data = record['MedlineCitation']['Article']['Journal']['JournalIssue']
+        self.journal_location = ''
+        if 'Volume' in journal_issue_data:
+            self.journal_location += journal_issue_data['Volume']
+        if 'Issue' in journal_issue_data:
+            self.journal_location += "(" + journal_issue_data['Issue'] + ")"
+        if ('Pagination' in record['MedlineCitation']['Article'] and
+            'MedlinePgn' in record['MedlineCitation']['Article']['Pagination']):
+            if self.journal_location:
+                self.journal_location += ':'
+            self.journal_location += record['MedlineCitation']['Article']['Pagination']['MedlinePgn']
+
+        if ('Abstract' in record['MedlineCitation']['Article'] and
+            'AbstractText' in record['MedlineCitation']['Article']['Abstract']):
+            self.abstract = ' '.join([x for x in
+                                      record['MedlineCitation']['Article']['Abstract']['AbstractText']])
+
+
 # Register model with reversion.
 reversion.register(Publication)
